6f5020343c90f51fb879690e8a4986ce577c745d
arka <arkakar1313@gmail.com>
2025-07-06 22:57:16 +0530

    Modified

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 ### Dotfiles for shared between various computers:
 1. Fedora workstation configuration in my thinkpad E16("nerdhut")
-2. Full installation of arch linux in lvm partition.
-3. Install and configured dwm with suckless tools.
--- docs/logical-volume-operations.sh
+++ docs/logical-volume-operations.sh
@@ -5,19 +5,19 @@ vgs
 lvs
 
 # Allocate extra space(10GB) from "volgroup0" to "root"
-lvextend -L +10g -n /dev/mapper/arch-root
+lvextend -L +10g -n /dev/mapper/fedora-root
 
 # The above command doesn't extend the space the "root" volume group:
 # To extend the partition:
-resize2fs /dev/mapper/arch-root
+resize2fs /dev/mapper/fedora-root
 
 # Note: Shrinking in lvm is a bad idea. Try to avoid it AMAP. It can be destructive:
 
 # Create a snapshot of the "root" volume:
-sudo lvcreate -L 5GB -s -n root-snap-19-08-2024 /dev/mapper/arch-root
+sudo lvcreate -L 5GB -s -n root-snap-19-08-2024 /dev/mapper/fedora-root
 
 # Restore a snapshot:
-sudo lvconvert --merge /dev/arch/root-snap-19-08-2024
+sudo lvconvert --merge /dev/fedora/root-snap-19-08-2024
 
 # Remove a snapshot:
-sudo lvremove /dev/arch/root-snap-19-08-2024
+sudo lvremove /dev/fedora/root-snap-19-08-2024
