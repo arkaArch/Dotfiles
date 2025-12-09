53e4bb4c40f9ff9221326f4e540441a67b0ec325
arka <arkakar1313@gmail.com>
2025-12-09 18:15:18 +0530

    Introduce arch hyprland

---
+++ docs/arch-hyprland-setup.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# Installing cli utilities:
+sudo pacman -S git openssh pacman-contrib
+
+# Instal fonts:
+sudo pacman -S noto-fonts{,-cjk,-emoji,-extra}
+sudo pacman -S ttf-cascadia-code-nerd
+
+# Install Hyprland and it's components:
+sudo pacman -S hyprland kitty hyprpaper rofi waybar swaync wl-clipboard
+ln -svf ~/Dotfiles/confs/{hypr,kitty} ~/.config
+
+# Browser:
+sudo pacman -S firefox 
+# Extension: ublock-origin
+
+# Installing sound drivers:
+sudo pacman -S pipewire pipewire-pulse
+
+# Installing file-managers:
+sudo pacman -S yazi ffmpeg 7zip jq poppler fd ripgrep fzf zoxide resvg imagemagick
+ln -svf ~/Dotfiles/confs/yazi ~/.config
+
+# Installing z-shell and exa:
+sudo pacman -S zsh zsh-{syntax-highlighting,autosuggestions}
+chsh -s $(which zsh)
+rm -r ~/.bash*
+ln -svf ~/Dotfiles/confs/zsh/zshrc_arch ~/.zshrc
+
+# Linking nvim config:
+ln -svf ~/Dotfiles/confs/nvim/ ~/.config/
