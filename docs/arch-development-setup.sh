9bfdf12d9d5c571df29f580bb9b774e70ee5c684
arka <arkakar1313@gmail.com>
2025-12-10 21:49:46 +0530

    Added

---
+++ docs/arch-development-setup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Install DrRacket:
+sudo pacman -S racket
+
+
+# Setup for python:
+#------------------------
+
+# Install pycharm:
+sudo pacman -S pycharm-community-edition
+
+# Install pip:
+sudo pacman -S python-pip
+
+# Install virtual environment:
+sudo pacman -S python-virtualenv
+
+# To install tkinter (for gui modules like FreeSampleGui to work):
+sudo pacman -S tk
