a42a4e2c835b9ebe081bf44a82c9b52e897295c3
arka <arkakar1313@gmail.com>
2025-12-11 13:38:34 +0530

    Fix pycharm warning msg for started it with a script

--- docs/arch-development-setup.sh
+++ docs/arch-development-setup.sh
@@ -9,6 +9,20 @@ sudo pacman -S racket
 
 # Install pycharm:
 sudo pacman -S pycharm-community-edition
+"""
+{
+    When open pycharm for first time you can see the notification in IDE that:
+        
+        - The IDE seems to be launched with a script launcher ('bin/pycharm.sh'). 
+        - Please consider switching to a native launcher ('bin/pycharm') for better experience
+    Now this happens because the IDE opens with pycharm script not the actual pycharm executable.
+
+    To solve this go to /usr/bin directory. Move the pycharm script with:
+        /usr/bin $ sudo mv pycharm pycharm.bak
+    Then symlink the executable file in the /usr/bin directory by:
+        /usr/bin $ sudo ln -sv /usr/share/pycharm/bin/pycharm pycharm
+}
+"""
 
 # Install pip:
 sudo pacman -S python-pip
