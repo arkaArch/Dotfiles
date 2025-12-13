c04d6735ecb2e1fa5c2b348f2b04df8237a97fbe
arka <arkakar1313@gmail.com>
2025-12-13 22:16:56 +0530

    enable tree sitter for python

--- confs/nvim/lua/plugins/treesitter.lua
+++ confs/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
     config = function()
         local config = require("nvim-treesitter.configs")
         config.setup({
-            ensure_installed = { "c", "cpp", "java", "lua" },
+            ensure_installed = { "c", "cpp", "python", "lua" },
             highlight = { enable = true },
             indent = { enable = true },
         })
