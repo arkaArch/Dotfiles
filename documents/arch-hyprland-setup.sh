7699947759edd2fd0932e693e76ef4cfce10195e
arka <arkakar1313@gmail.com>
2026-01-17 19:18:24 +0530

    Changes folder structures and names

--- documents/arch-hyprland-setup.sh
+++ documents/arch-hyprland-setup.sh
@@ -6,7 +6,7 @@ sudo pacman -S ttf-cascadia-code-nerd ttf-firacode-nerd
 
 # Install Hyprland and it's components:
 sudo pacman -S hyprland kitty hyprpaper hyprlock htpridle rofi mako waybar wl-clipboard
-ln -svf ~/Dotfiles/confs/{hypr,kitty,rofi,mako,waybar} ~/.config
+ln -svf ~/Dotfiles/config/{hypr,kitty,rofi,mako,waybar} ~/.config
 
 # Browser:
 sudo pacman -S firefox 
@@ -17,27 +17,27 @@ sudo pacman -S pipewire pipewire-pulse sof-firmware brightnessctl
 
 # Installing file-managers:
 sudo pacman -S yazi ffmpeg 7zip jq poppler fd ripgrep fzf zoxide resvg imagemagick udisks2
-ln -svf ~/Dotfiles/confs/yazi ~/.config
+ln -svf ~/Dotfiles/config/yazi ~/.config
 sudo pacman -S thunar gvfs gvfs-mtp thunar-archive-plugin thunar-media-tags-plugin thunar-volman tumbler ffmpegthumbnailer
 
 # Installing z-shell and exa:
 sudo pacman -S exa zsh zsh-{syntax-highlighting,autosuggestions}
 chsh -s $(which zsh)
 rm -r ~/.bash*
-ln -svf ~/Dotfiles/confs/zsh/zshrc ~/.zshrc
+ln -svf ~/Dotfiles/config/zsh/zshrc ~/.zshrc
 
 # Linking nvim config:
-ln -svf ~/Dotfiles/confs/nvim/ ~/.config/
+ln -svf ~/Dotfiles/config/nvim/ ~/.config/
 
 # Install image-viewer music and video player:
 sudo pacman -S ristretto mpv mpd ncmpcpp
-ln -svf ~/Dotfiles/confs/{mpv,mpd,ncmpcpp} ~/.config
+ln -svf ~/Dotfiles/config/{mpv,mpd,ncmpcpp} ~/.config
 systemctl --user enable mpd
 systemctl --user start mpd
 
 # Installing cli utilities:
 sudo pacman -S git openssh pacman-contrib tree fastfetch fwupd unzip
-ln -svf ~/Dotfiles/confs/fastfetch ~/.config
+ln -svf ~/Dotfiles/config/fastfetch ~/.config
 
 # GTK look and feel:
 sudo pacman -S nwg-look materia-gtk-theme papirus-icon-theme
