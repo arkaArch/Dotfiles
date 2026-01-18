dfe1c912a807877c13be2dab250ac9ac1edabbb9
arka <arkakar1313@gmail.com>
2026-01-18 18:45:18 +0530

    configs changed to config

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-        local config = require("nvim-treesitter.configs")
+        local config = require("nvim-treesitter.config")
         config.setup({
             ensure_installed = { "bash", "c", "cpp", "lua" },
             highlight = { enable = true },
