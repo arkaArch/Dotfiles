d2f32016b844097c274ca7cbc64dac0ab8ed249b
arka <arkakar1313@gmail.com>
2026-01-19 17:16:30 +0530

    Remove some unwanted

--- documents/git-initial-configurations.sh
+++ documents/git-initial-configurations.sh
@@ -33,36 +33,3 @@ ssh-keygen -t ed25519 -C "arkakar1313@gmail.com"
 ssh -T git@github.com
 # press yes, and then your key password...You see the msg as:
 # Hi arkaArch! You've successfully authenticated, but GitHub does not provide shell access.
-
-
-
-
-
-### Git commands and tests:
-
-# Make a github repo Git-Test. Create a Readme there. 
-# Copy ssh path and clone it with
-git clone git@github.com:arkaArch/Git-Test.git
-cd Git-Test
-touch helloGit.txt
-git status
-
-# Notice helloGit.txt file is shown in red, which means that this file is not staged.
-# Add helloGit.txt file to the staging area in Git.
-git add helloGit.txt
-git add . 
-# To add all files in the current directory and all subsequent directories to the staging area.
-
-
-# Now, type git status again. In the output, notice that your file is now shown in green, 
-# which means that this file is now in the staging area.
-git commit -m "Added helloGit"
-
-# If you now check git status you'll see "Your branch is ahead of 'origin/main' by 1 commit."
-# That means, you now have newer snapshots than what is on your remote repository. 
-
-# To see activities:
-git log
-
-# Now push it in remote with:
-git push origin main/ git push
